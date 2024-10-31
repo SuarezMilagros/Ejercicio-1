630be30e52cb9f3a0b0973aafa06a1db59ab543b
SuarezMilagros <milagros.16.princesa@gmail.com>
2024-10-31 01:03:02 -0300

    Evento y metodos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   mostrarSaludo: boolean = true;
+
+  toggleSaludo() {
+    this.mostrarSaludo = !this.mostrarSaludo;
+}
 }
