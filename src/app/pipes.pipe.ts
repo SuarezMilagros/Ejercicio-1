efa860d5dfefdd3ec31285d4ff868a1ba50c0a7b
SuarezMilagros <milagros.16.princesa@gmail.com>
2024-10-31 01:23:47 -0300

    Pipes

---
+++ src/app/pipes.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mayusculas'
+})
+export class MayusculasPipe implements PipeTransform {
+  transform(value: string): string {
+    return value ? value.toUpperCase() : '';
+  }
+}
+
+
+
+
