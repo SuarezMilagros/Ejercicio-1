a725006eb28bc57ddff9d3d6585229bec5ac3888
SuarezMilagros <milagros.16.princesa@gmail.com>
2024-10-31 00:58:23 -0300

    Directiva ngClass

--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -8,4 +8,18 @@ import { Component, Input } from '@angular/core';
 export class SaludoComponent {
   nombre: string = 'Milagros';
   @Input() mostrarSaludo: boolean = true;
+
+  color: string = '';
+  rojo: boolean = false;
+  verde: boolean = false;
+
+  ngOnInit(): void {
+  }
+
+  esRojo(){
+    this.rojo = !this.rojo;
+  }
+  esVerde(){
+    this.verde = !this.verde;
+  }
 }
